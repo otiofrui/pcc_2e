79d4c8c9d32c10efdf70d99d190837f72418f808
otiofrui <spam@dev.otiofrui.org>
2021-10-29 17:43:46 +0200

    chapter 01 02

---
+++ mycode/mycode.py
@@ -0,0 +1,34 @@
+# personal cheat sheet
+
+the_answer=42
+print(the_answer)
+the_answer="fourty two"
+print(the_answer)
+
+lwrcase_text="das SOLLTE Eigentlich aLLes groß sein am Anfang oder klein.. keine Ahnung"
+print(lwrcase_text.title())
+print(lwrcase_text.lower())
+print(lwrcase_text.upper())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+x = 2*3
+print(f"Mult: {x}")
+print(f"Exp: {2**3}")
+
+big_number=20_000_000_000
+print(big_number)
+
+x,y,z=0,0,0
+print(x, y, z)
+
+import this
