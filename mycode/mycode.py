d7f8815ae2e777f1418cfdc16c1db26af6bfa055
otiofrui <spam@dev.otiofrui.org>
2021-10-30 17:24:53 +0200

    chapter 05

--- mycode/mycode.py
+++ mycode/mycode.py
@@ -80,3 +80,56 @@ print(squares[3:])
 tupels = (10, 20, 30)
 for tupel in tupels:
     print(tupel)
+
+car = 'audi'
+print(car == 'bmw')
+
+age_0 = 22
+age_1 = 18
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+switch_on=False
+number_switch=1
+ipf = range(1,11,2)
+pp = range(100, 210, 10)
+if switch_on is True:
+    for i in ipf:
+        for p in pp:
+            d=p*i
+            print (d-1)
+elif number_switch is 0:
+    print("Number Switch is off too!")
+else:
+    print("Switches are off!")
+
+# Pizza Orders
+
+print("\nPizza Orders\n")
+
+green_peppers=5
+print(green_peppers)
+#ushrooms=2
+#extra_cheese=1
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            if green_peppers is 0:
+                print("Sorry, we are out of green peppers right now.")
+            else:
+             print(f"Adding {requested_topping}.")
+             green_peppers = green_peppers-1
+        else:
+             print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+print(green_peppers)
